5b7cc7ed070ee2f80e07320393b769961d61bc5d
google-automerger <automerger@google.com>
2015-03-30 17:43:01 -0700

    Auto-update

--- Application/tests/src/com/example/android/basicnetworking/tests/SampleTests.java
+++ Application/tests/src/com/example/android/basicnetworking/tests/SampleTests.java
@@ -43,7 +43,6 @@ import android.test.ActivityInstrumentationTestCase2;
 public class SampleTests extends ActivityInstrumentationTestCase2<MainActivity> {
 
     private MainActivity mTestActivity;
-    private BasicNetworkingFragment mTestFragment;
 
     public SampleTests() {
         super(MainActivity.class);
@@ -58,8 +57,6 @@ public class SampleTests extends ActivityInstrumentationTestCase2<MainActivity> 
         // flags = {@link Intent#FLAG_ACTIVITY_NEW_TASK}
         // All other fields are null or empty.
         mTestActivity = getActivity();
-        mTestFragment = (BasicNetworkingFragment)
-            mTestActivity.getSupportFragmentManager().getFragments().get(1);
     }
 
     /**
@@ -69,7 +66,6 @@ public class SampleTests extends ActivityInstrumentationTestCase2<MainActivity> 
         //Try to add a message to add context to your assertions. These messages will be shown if
         //a tests fails and make it easy to understand why a test failed
         assertNotNull("mTestActivity is null", mTestActivity);
-        assertNotNull("mTestFragment is null", mTestFragment);
     }
 
     /**
